edd9dadc6533832c413cf8c10babe0b484eb2863
xingaqr <xingaqr@gmail.com>
2021-01-13 13:11:01 +0800

    add FriendLink Hackerchai

--- docs/.vuepress/friendLink.js
+++ docs/.vuepress/friendLink.js
@@ -11,18 +11,16 @@ module.exports = [
     title: 'weearc的个人博客',
     desc: '相离相惜莫相忘，且行且歌且珍惜',
     email: 'weearc@qq.com',
-    link: 'https://blog.weearc.top',
+    link: 'https://blog.weearc.top/',
   },
   {
     title: 'ReaL',
     desc: '人生若江湖，一晌贪欢。',
-    link: 'https://rea.ink',
+    link: 'https://rea.ink/',
     avatar: 'https://www.rea.ink/head.png',
   },
   {
-    title: '落萧的编码小记',
-    desc: '信息论、汉字学与输入方案',
-    link: 'https://lanluoxiao.github.io',
-    avatar: 'https://lanluoxiao.github.io/images/avatar.JPG',
+    title: 'Hackerchai IT blog',
+    link: 'https://blog.hackerchai.com/',
   },
 ]
